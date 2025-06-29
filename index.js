29d0fdf2177e0e0f8dbccc5a1660f7051a38b8dd
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-06-29 11:56:23 +0600

    api pass change

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ app.get("/api/nid", async (req, res) => {
     const fetch = (await import("node-fetch")).default;
 
     const response = await fetch(
-      `https://api.taka0nai.com/Json.php?key=MoNiR&pass=MoNiR123&nid=${nid}&dob=${dob}`
+      `https://api.taka0nai.com/Json.php?key=MoNiR&pass=MoNiR321&nid=${nid}&dob=${dob}`
       // `https://api.taka0nai.com/Json.php?key=MoNiR&nid=${nid}&dob=${dob}`
       // `https://backup.taka0nai.my.id/Json.php?key=MoNiR&nid=${nid}&dob=${dob}`
     );
