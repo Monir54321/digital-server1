4acb10583f6e196e4254c1c9802e2df2a88db2f0
suhanur-rohoman-sumon-github <sumonsuhanurrohoman@gmail.com>
2025-08-07 04:23:54 -0700

    some more changes

--- bot/whatsapp.js
+++ bot/whatsapp.js
@@ -52,8 +52,11 @@ const client = new Client({
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
       "--disable-accelerated-2d-canvas",
-      "--no-zygote",
       "--disable-gpu",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions",
     ],
   },
 });
